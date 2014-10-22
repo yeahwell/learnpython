25bddc20cfbb9396e3a2953e77bd4d294fd407c9
yeahwell <yeahwell19920525@gmail.com>
2014-10-22 20:27:14 +0800

    module of python

---
+++ course/module-future.py
@@ -0,0 +1,15 @@
+﻿
+# still running on Python 2.7
+
+from __future__ import unicode_literals
+from __future__ import division
+
+print isinstance('xxx', unicode)
+print isinstance(u'xxx', unicode)
+print isinstance('xxx', str)
+print isinstance(b'xxx', str)
+
+#Python 3.x中，所有的除法都是精确除法，地板除用//表示
+print 10 / 3
+print 10.0 / 3
+print 10 // 3
---
+++ course/module-install.py
@@ -0,0 +1,25 @@
+﻿
+# 安装easy_install
+###
+#1. 下载ez_setup.py，python ez_setup.py
+#2. 请添加C:\Python27\Scripts到环境变量Path
+#3. 在pypi.python.org中找到Python Imaging Library，
+###
+
+import Image
+im = Image.open('test.jpg')
+print im.format, im.size, im.mode
+#im.thumbnail((200, 100))
+#im.save('thumb.png', 'png')
+
+#import mymodule
+
+#默认情况下，Python解释器会搜索当前目录、所有已安装的内置模块和第三方模块，
+#搜索路径存放在sys模块的path变量中
+import sys
+print sys.path
+
+# 添加自己的搜索目录
+# 直接修改sys.paht。运行时修改，运行结束后失效。
+import sys
+sys.path.append('/Users/yeahwell/Documents/R')

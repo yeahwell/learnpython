ccdef2763c66a7d25633e679478cc1901f130ac7
yeahwell <yeahwell19920525@gmail.com>
2014-11-04 11:04:40 +0800

    add io and email of python

---
+++ course/io/test-file-dictionary.py
@@ -0,0 +1,28 @@
+﻿
+import os
+print os.name
+#如果是posix，说明系统是Linux、Unix或Mac OS X，如果是nt，就是Windows系统。、
+
+#获取详细的系统信息
+#print os.uname()
+print os.environ
+print os.getenv('PATH')
+
+#操作文件和目录
+print os.path.abspath('.')
+print os.path.join(os.path.abspath('.'), 'testdir')
+#os.mkdir(os.path.join(os.path.abspath('.'), 'testdir'))
+#os.rmdir(os.path.join(os.path.abspath('.'), 'testdir'))
+
+print os.path.split('test.txt')
+print os.path.splitext('test.txt')
+
+#os.rename('test.txt', 'test.py')
+#os.remove('test.py')
+
+#列出当前目录下的所有目录
+print [x for x in os.listdir('.') if os.path.isdir(x)]
+#列出所有的.py文件
+print [x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1] == '.py']
+
+
---
+++ course/io/test-read-write.py
@@ -0,0 +1,29 @@
+﻿
+#读文件
+#由于文件读写时都有可能产生IOError，一旦出错，后面的f.close()就不会调用。所以，为了保证无论是否出错都能正确地关闭文件，我们可以使用try ... finally来实现：
+try:
+	f = open('test.txt', 'r')
+	print f.read()
+finally:
+	if f:
+		f.close()
+
+#Python引入了with语句来自动帮我们调用close()方法：
+with open('test.txt', 'r') as f:
+	print f.read()
+
+with open('test.txt', 'r') as f:
+	for line in f.readlines():
+		print(line.strip()) #把末尾的'\n'删掉
+
+#读二进制文件
+#f = open('test.jpg', 'rb')
+#print f.read()
+
+#f = open('gbk.txt', 'rb')
+#u = f.read().decode('gb2312')
+#u
+#print u
+
+with open('test.txt', 'w') as f:
+	f.write('wesley')
